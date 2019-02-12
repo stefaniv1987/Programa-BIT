197134dc392215d20bfa3bedd4a4db3603433eb0
stefaniv1987@gmail.com <stefaniv1987@gmail.com>
2019-02-11 22:59:34 -0300

    cambio en el end point del js

--- src/main/resources/public/dada.js
+++ src/main/resources/public/dada.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://localhost:8080/smartParking/disponibilidad"
+       // url: "http://localhost:8080/smartParking/disponibilidad"
+          url: "http://smart-bici.herokuapp.com/smartParking/disponibilidad"   
     }).then(function(data) {
        $('.dios').append(data);
     });
