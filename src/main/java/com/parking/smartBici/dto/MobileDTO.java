8b0382d16051100a49a76101f85a2ad12e1457b3
stefaniv1987 <sviera@altimetrik.com>
2020-02-24 16:31:06 -0300

    dto no muestre nulos

--- src/main/java/com/parking/smartBici/dto/MobileDTO.java
+++ src/main/java/com/parking/smartBici/dto/MobileDTO.java
@@ -1,5 +1,9 @@
 package com.parking.smartBici.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class MobileDTO {
 	
 private Integer lugaresDisponibles;
